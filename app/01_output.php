2770d057c6ebc616da62ec6a68cc709874d5cf59
Michal <michalgrzegorczyk04@gmail.com>
2024-01-27 13:57:16 +0100

    output

--- app/01_output.php
+++ app/01_output.php
@@ -1,8 +1,36 @@
 <?php
 /* -------- Output & Comments ------- */
+// echo 123, 'Hello, 10.5';
 
-echo "Test";
+// print 123;
+// print_r([1, 2, 3]);
+
+// var_dump('Hello');
+// var_dump(true);
+// var_export('hello');
 
 /*
   A .php file running on a server, can output both HTML and PHP code. There are multiple functions that can be used to output data to the browser.
 */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>
+    <?php echo 'Post one'; ?>
+  </h1>
+  <h2>
+    <?= 'Post two'; ?>
+  </h2>
+</body>
+
+</html>
