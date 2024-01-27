fc94c1d88511345ba6af0208cda153c392c95609
Michal <michalgrzegorczyk04@gmail.com>
2024-01-27 14:07:57 +0100

    variables

--- app/02_variables.php
+++ app/02_variables.php
@@ -22,3 +22,32 @@
 - Variables can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
 - Variables are case-sensitive ($name and $NAME are two different variables)
 */
+
+$name = 'Michał';
+$age = 30;
+$has_kids = false;
+$cash_on_hand = 20.75;
+
+// echo $name;
+// echo '<br/>';
+// echo $age;
+// echo '<br/>';
+// var_dump($has_kids);
+// echo '<br/>';
+// echo $cash_on_hand;
+
+// echo "$name is $age years old<br/>";
+// echo $name . ' is ' . $age . ' years old<br/>';
+// echo "${name} is ${age} years old<br/>"; // deprecated
+
+$x = '5' + 5;
+// var_dump($x);
+// echo 10 -5;
+// echo 5 * 6;
+// echo 10 / 3;
+// echo 10 % 4;
+
+define('HOST', 'localhost');
+define('DB_NAME', 'dev_db');
+
+echo HOST;
