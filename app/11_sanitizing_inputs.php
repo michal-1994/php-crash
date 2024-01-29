67ba2bda1f5b5e4262c401f70955a3943b94ff8e
Michal <michalgrzegorczyk04@gmail.com>
2024-01-29 13:47:54 +0100

    sanitizing inputs

--- app/11_sanitizing_inputs.php
+++ app/11_sanitizing_inputs.php
@@ -1,6 +1,29 @@
-<?php
-/* --- Sanitizing Inputs -- */
+<a href="<?php echo $_SERVER['PHP_SELF']; ?>?name=John&age=30">Click</a>
+<br />
 
-/*
-  Data submitted through a form is not sanitized by default. We have methods to sanitize data manually.
-*/
+<?php
+
+if (isset($_POST['submit'])) {
+  // $name = htmlspecialchars($_POST['name']);
+  // $age = htmlspecialchars($_POST['age']);
+  $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+  $age = filter_input(INPUT_POST, 'age', FILTER_SANITIZE_SPECIAL_CHARS);
+
+  // $name = filter_var($_POST['name'], FILTER_SANITIZE_SPECIAL_CHARS);
+
+  echo $name . ' - ' . $age . '<br />';
+}
+
+?>
+
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+  <div>
+    <label for="name">Name: </label>
+    <input type="text" name="name" />
+  </div>
+  <div>
+    <label for="age">Age: </label>
+    <input type="text" name="age" />
+  </div>
+  <input type="submit" value="Submit" name="submit" />
+</form>
