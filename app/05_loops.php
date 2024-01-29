7e56ec34ee7fe71fe3e0f7f7751e2cedec0af3bf
Michal <michalgrzegorczyk04@gmail.com>
2024-01-29 12:08:54 +0100

    loops

--- app/05_loops.php
+++ app/05_loops.php
@@ -43,3 +43,42 @@ do...while loop will always execute the block of code once, even if the conditio
   // code to be executed
   }
 */
+
+// for ($x = 0; $x <= 10; $x++) {
+//   echo "Number is $x";
+//   echo "<br/>";
+// }
+
+$y = 2;
+
+// while ($y <= 15) {
+//   echo "Number $y <br />";
+//   $y = $y + 2;
+// }
+
+$z = 1;
+
+// do {
+//   echo "$z <br />";
+//   $z++;
+// } while ($z <= 10);
+
+// $array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+$array = ['one', 'two', 'three'];
+
+// foreach ($array as $value) {
+//   echo "Number $value <br />";
+// }
+
+// for ($i = 0; $i < count($array); $i++) {
+//   echo "Number is " . $array[$i];
+//   echo "<br/>";
+// }
+
+// foreach ($array as $item) {
+//   echo $item . "<br />";
+// }
+
+// foreach($array as $key => $value) {
+//   echo "Key: $key <br />Value: $value <hr />";
+// }
