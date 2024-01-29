61de1b05deadb8620ebcc6d13d2facc246292b07
Michal <michalgrzegorczyk04@gmail.com>
2024-01-29 12:17:17 +0100

    functions

--- app/06_functions.php
+++ app/06_functions.php
@@ -12,3 +12,32 @@
     // code to be executed
   }
 */
+
+$y = 12;
+$email = 'test@test.pl';
+// function registerUser($email) {
+//   // global $y;
+//   // echo $y;
+//   echo "$email registered";
+// }
+
+// registerUser($email);
+
+// function sum($n1 = 1, $n2 = 2)
+// {
+//   return $n1 + $n2;
+// }
+
+// $number = sum();
+
+// echo $number;
+
+// $subtract = function($n1, $n2) {
+//   return $n1 - $n2;
+// };
+
+// echo $subtract(10, 5);
+
+// $multiply = fn($n1, $n2) => $n1 * $n2;
+
+// echo $multiply(9, 5);
