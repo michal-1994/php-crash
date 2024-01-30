d8fb3cc1a7e8873b3da31d932a0ef3447dad460c
Michal <michalgrzegorczyk04@gmail.com>
2024-01-30 10:21:21 +0100

    cookies

--- app/12_cookies.php
+++ app/12_cookies.php
@@ -1,6 +1,7 @@
 <?php
-/* ------------- Cookies ------------ */
 
-/*
-  Cookies are a mechanism for storing data in the remote browser and thus tracking or identifying return users. You can set specific data to be stored in the browser, and then retrieve it when the user visits the site again.
-*/
+setcookie("name", "Brad", time() + 864);
+
+if (isset($_COOKIE["name"])) {
+  echo $_COOKIE["name"];
+}
